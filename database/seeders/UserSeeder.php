197414d5ac9ad00435bf23af101ccfc10e20f8a5
unknown <christianjohnruiz395@gmail.com>
2025-09-19 20:43:35 +0800

    updated seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,23 +5,33 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'name' => 'Admin',
+
+         User::updateOrCreate(
+            ['email' => 'admin@minimart.com'],
+            [
+                 'name' => 'Admin',
             'email' => 'admin@minimart.com',
             'password' => Hash::make('password'),
             'role' => 'admin',
-        ]);
+                'remember_token' => Str::random(10),
+            ]
+        );
 
-        User::create([
-            'name' => 'Staff User',
-            'email' => 'staff@example.com',
+        // Staff user
+        User::updateOrCreate(
+            ['email' => 'staff@minimart.com'],
+            [
+                 'name' => 'Staff',
+            'email' => 'staff@minimart.com',
             'password' => Hash::make('password'),
-            'role' => 'staff',
-        ]);
+                'remember_token' => Str::random(10),
+            ]
+        );
     }
 }
